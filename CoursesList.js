caf5077d56978fdc7a13362b9089adf73eb38107
HHolben <47185193+HHolben@users.noreply.github.com>
2024-06-22 22:31:34 -0400

    Add files via upload

---
+++ CoursesList.js
@@ -0,0 +1,143 @@
+
+var Courses =
+[
+   /* {
+        "Name": "Successful Paragraphs",
+        "Type": "SummerIntensive",
+        "Description":  "Six Two-hour sessions designed to help students learn to write effective paragraphs.",
+        "TargetGradeLevels":  "6/7",
+        "Meetings":  "Mondays and Wednesdays from 8:00 a.m.-10:00a.m. (EST)",
+        "Duration":  "June 10-28",
+        "Price":  "$120.00",
+        "CourseLink":  "linkHere.link",
+        "TileImage": "ImageLinkHere.jpg"
+    },
+
+    {
+        "Name":  "Writing Essays—Basic Training",
+        "Type": "SummerIntensive",
+        "Description":  "Six Two-hour sessions provide students with the tools for developing three-paragraph expository essays.",
+        "TargetGradeLevels":  "7/8",
+        "Meetings":  "Mondays and Wednesdays from 10:15 a.m. - 12:15 p.m. (EST)",
+        "Duration":  "June 10-28",
+        "Price":  "$120.00",
+        "CourseLink":  "linkHere.link",
+        "TileImage": "ImageLinkHere.jpg"
+    },
+
+
+    {
+        "Name":  "Writing Essays—Advanced Training",
+        "Type": "SummerIntensive",
+        "Description":  "Six Two-hour sessions train students to use effective strategies while building five-paragraph expository essays.",
+        "TargetGradeLevels":  "7/8/9",
+        "Meetings":  "Tuesdays and Thursdays from 8:00 a.m.-10:00 a.m.(EST)",
+        "Duration":  "June 10-28",
+        "Price":  "$120.00",
+        "CourseLink":  "linkHere.link",
+        "TileImage": "ImageLinkHere.jpg"
+    },
+
+
+    {
+        "Name":  "Advanced Essay Development",
+        "Type": "Summer Intensive",
+        "Description":  "Six Two-Hour sessions help student use specific writing traits and rhetoric to develop five-paragraph essays.",
+        "TargetGradeLevels":  "9/10",
+        "Meetings":  "Tuesdays and Thursdays from 10:15 a.m.- 12:15 p.m. (EST)",
+        "Duration":  "June 10-28",
+        "Price":  "$120.00",
+        "CourseLink":  "linkHere.link",
+        "TileImage": "ImageLinkHere.jpg"
+    },
+
+    {
+        "Name":  "Creative Writing I: Section 1: Middle Grades",
+        "Type": "Summer Intensive",
+        "Description":  "Six 90-minute sessions enable students to find their creative voices. Students will finish intensive with one piece of writing ready to enter in competitions. Students will be provided with a list of suitable competitions for the 2024-25 school term at the end of the course.",
+        "TargetGradeLevels":  "Middle Grades",
+        "Meetings":  "Mondays and Wednesdays from 8:00 a.m.-9:30 a.m. (EST).",
+        "Duration":  "July 8-25",
+        "Price":  "$100.00",
+        "CourseLink":  "linkHere.link",
+        "TileImage": "ImageLinkHere.jpg"
+    },
+    
+
+    {
+        "Name":  "Creative Writing I: Section 2: High School",
+        "Type": "Summer Intensive",
+        "Description":  "Six 90-minute sessions enable students to find their creative voices. Students will finish intensive with one piece of writing ready to enter in competitions. Students will be provided with a list of suitable competitions for the 2024-25 school term at the end of the course.",
+        "TargetGradeLevels":  "High School",
+        "Meetings":  "Mondays and Wednesdays 9:45 - 11:15 a.m. (EST)",
+        "Duration":  "July 8-25",
+        "Price":  "$100.00",
+        "CourseLink":  "linkHere.link"
+    },
+
+    */
+
+    {
+        "Name":  "English I",
+        "Type": "Fall/Spring",
+        "Description":  "Students will approach the language arts as the study of God’s gift of language. They will begin the transfer from a reading emphasis to a more in depth study of literature. Composition and study skills will be an integral part of the course; grammar will be studied as a tool for composition and speaking skills",
+        "TargetGradeLevels":  "7",
+        "Meetings":  "Mondays and Wednesdays 8:00 - 9:30 a.m. (EST)",
+        "Duration":  "June 8-25",
+        "Price":  "$300.00",
+        "CourseLink":  "English1.html"
+    },
+
+
+    
+    {
+        "Name":  "English II",
+        "Type": "Fall/Spring",
+        "Description":  "This course is designed to provide some extra help with understanding our language. No homework is assigned with course. We work together developing strategies for a stronger application of grammar with our compositions. Fall Term only",
+        "TargetGradeLevels":  "8",
+        "Meetings":  "Tuesdays and Thursdays 9:45 - 11:15 a.m.(EST)",
+        "Duration":  "August - May",
+        "Price":  "$300.00",
+        "CourseLink":  "English2.html"
+    },
+
+    {
+        "Name":  "Grammar Supplement I",
+        "Type": "Fall/Spring",
+        "Description":  "Students will continue to develop in their study of God’s gift of language. Students will work on speed and fluency with reading challenging texts. Composition and study skills will be an integral part of the course; grammar will be studied as a tool for composition and speaking skills",
+        "TargetGradeLevels":  "7/8",
+        "Meetings":  "Thursdays",
+        "Duration":  "August - December",
+        "Price":  "$150.00",
+        "CourseLink":  "linkHere.link"
+    },
+
+
+
+    {
+        "Name":  "American History, Literature, and Composition",
+        "Type": "Fall/Spring",
+        "Description":  "* Fall term only. The focus of AHLC is to provide a rhetoric-level study of the history of the United States, with an emphasis on the humanities. Research, oral communication, and composition are integral components of the course; scored discussion, oral discussion and argument are frequent activities. We begin with a review of a biblical worldview as it relates to history, first understanding that history is linear, with creation the beginning, heaven the end, and Christ the center. We then apply biblical principles to the founding and development of our country, as well as to contemporary issues. Of special emphasis is the Creation, Fall, Redemption paradigm as it relates to the philosophical decline of the US and to the errors of revisionist history.",
+        "TargetGradeLevels":  "11",
+        "Meetings":  "Mondays and Wednesdays 8:00 - 9:30 p.m. (EST)",
+        "Duration":  "June 8 - 25",
+        "Price":  "$550.00",
+        "CourseLink":  "AmericanSurveyAmericanHistoryLiteratureAndCompositionFrom1940ToPresent.html"
+    },
+
+    {
+        "Name":  "World History II Literature and Composition",
+        "Type": "Fall/Spring",
+        "Description":  "Our primary focus is to understand God’s purpose for history. His plan is revealed to us through written records.      Starting with the premises that history is the story of God’s plan for man and that history is linear, with Christ and the cross at the center, the students trace the development of empires, nations, countries, philosophies, movements,  and trends. World history is an overview from the age of exploration to modern times. This course integrates the humanities—art, literature, music and architecture. Writing skills are utilized as students spend time in historical research, analyzing primary sources, and communicating a biblical world view of historical events and interpretations.",
+        "TargetGradeLevels":  "Middle Grades",
+        "Meetings":  "Mondays and Wednesdays 11:30-1:00 p.m. (EST)",
+        "TargetGradeLevels":  "10",
+        "Duration":  "June 8 - 25",
+        "Price":  "$550.00",
+        "CourseLink":  "WorldHistory2LiteratureAndComposition.html"
+    }
+
+      
+
+
+];
